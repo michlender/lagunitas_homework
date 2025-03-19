7001a5b40c7781e8028cc8a4178b0a5eb2a0ab9a
Michele Collender <michelecollender@Mac.lan>
2025-03-19 16:26:27 -0700

    Increase max_digits for score field

--- ratings/models.py
+++ ratings/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Rating(models.Model):
     beer_name = models.TextField()
-    score = models.DecimalField(max_digits=2, decimal_places=1)
+    score = models.DecimalField(max_digits=5, decimal_places=1)
     notes = models.TextField(blank=True)
     brewer = models.TextField(blank=True)
